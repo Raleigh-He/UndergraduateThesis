b4c5c42e861366c626a0b145f2ad044e17849445
Raleigh-He <89024742+Raleigh-He@users.noreply.github.com>
2021-09-08 10:16:28 +0800

    Revised_Bian

--- TimeProp_cmplx.f90
+++ TimeProp_cmplx.f90
@@ -1,6 +1,6 @@
 program main
     implicit none
-    real*8,parameter:: dp=0.01,dt=0.1,Pc=5.0
+    real*8,parameter:: dp=0.01,dt=0.1,Pc=6.0
     integer n
     n=Pc/dp
     call TimeProp(dt,dp,n)
@@ -10,11 +10,11 @@ end program
 subroutine TimeProp(dt,dp,n)
     implicit none
     integer,parameter :: Nmax=1000
-    real*8,parameter:: E0=5.337e-4,omg=2.85e-2,tau=1102,pi=4.0*datan(1.0d0)
+    real*8,parameter:: E0=5.337e-2,omg=5.7e-2,tau=1102,pi=4.0*datan(1.0d0)
     real*8 dt,dp,t,At
-    complex B_prime(Nmax,Nmax)
+    double complex B_prime(Nmax,Nmax)
     !Psi即含时波函数,A-算符A;B-算符B;D-算符p+A(t)
-    complex,allocatable :: dv(:),Psi(:),Psi1(:),A(:,:),B(:,:),D(:,:)
+    double complex,allocatable :: dv(:),Psi(:),Psi1(:),A(:,:),B(:,:),D(:,:),D2(:,:)
     integer n,m,i,j !m-时间轴的格数,n-p轴格数
     character PsiIn*12,NameA*15
     m=floor(tau/dt)
@@ -24,34 +24,42 @@ subroutine TimeProp(dt,dp,n)
     allocate(A(1:n+1,1:n+1))
     allocate(B(1:n+1,1:n+1))
     allocate(D(1:n+1,1:n+1))
+    allocate(D2(1:n+1,1:n+1))
     
     !读取初始波函数
-    print "(a)","请指定初始波函数的文件名："
-    read(*,*) PsiIn
-    open(10,file=PsiIn)
-    read(10,*) (Psi(i),i=1,n+1)
+ !   print "(a)","请指定初始波函数的文件名："
+ !   read(*,*) PsiIn
+    open(10,file='phi1.dat')
+    do i=1,n+1
+    read(10,*) Psi(i)
+    enddo
     close(10)
     
     !读取算符A=exp(=i*H0*0.5dt)
     !print "(a)" "请指定算符A的输入文件名："
     !NameA="OperatorA.txt"
-    NameA="A_cmplx.txt"
-    open(11,file=NameA)
+    NameA="OA.txt"
+    open(11,file='OA.txt')
     do i=1,n+1
-    read(11,*) (A(i,:))
+    do j=1,n+1
+    read(11,*) A(i,j)
     end do
+    enddo
     close(11)
     !算符D
     do i=1,n+1
-        D(i,i)=i*dp
+        D(i,i)=(i-n/2-1)*dp    !i*dp
     end do
     B=0
     !含时演化
     do j=0,m
        t=j*dt 
        At=E0*sin(t*pi/tau)**2*cos(omg*t)/omg !At=A(t)
-       D=D+At
-       dv(j)=dot_product(Psi,matmul(D,Psi))*dp
+       do i=1,n+1
+       D2(i,i)=D(i,i)+At
+       enddo
+
+       dv(j)=dot_product(Psi,matmul(D2,Psi))  !*dp
        call OperatorB(dp,dt,t,n,B_prime)
        do i=1,n+1
            B(i,i)=B_prime(i,i)
@@ -76,16 +84,16 @@ subroutine OperatorB(dp,dt,t,n,B)
     integer i,Nmax,n
     real*8 E0,omg,tau,pi
     parameter (Nmax=1000)
-    parameter (E0=5.337e-4,omg=2.85e-2,tau=1102,pi=4.0*datan(1.0d0))
-    complex HI(Nmax,Nmax),B(Nmax,Nmax)
+    parameter (E0=5.337e-2,omg=5.7e-2,tau=1102,pi=4.0*datan(1.0d0))
+    double complex HI(Nmax,Nmax),B(Nmax,Nmax)
     real*8 dp,dt,t
     real*8 At
-    complex,parameter :: cj=(0.,1.)
+    double complex,parameter :: cj=(0.,1.)
     At=E0*sin((t+dt/2)*pi/tau)**2*cos(omg*(t+dt/2))/omg !At=A(t+dt/2)
     HI=0
     B=0
     do i=1,n+1
-        HI(i,i)=i*dp*At+At**2/2 !HI(i,i)=pi*A(t)+A(t)^2/2
+        HI(i,i)=(i-n/2-1)*dp*At+At**2/2 !HI(i,i)=pi*A(t)+A(t)^2/2
     end do
     do i=1,n+1
         B(i,i)=exp(-cj*dt*HI(i,i))
